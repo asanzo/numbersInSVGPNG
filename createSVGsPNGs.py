190aec8ff1a35fe1fc492982da368bb46bb4983d
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-07-18 00:34:30 -0300

    removing trailing zeroes from image

--- createSVGsPNGs.py
+++ createSVGsPNGs.py
@@ -4,15 +4,15 @@ import os
 currentPath = os.path.dirname(os.path.realpath(__file__))
 modelSvgPath = os.path.join(currentPath, '999.svg')
 
-def threeDigits(number):
+def threeDigits(number, filling):
 	if(number < 10):
-		return '00'+str(number)
+		return filling + filling + str(number)
 	if(number < 100):
-		return '0'+str(number)
+		return filling + str(number)
 	return str(number)
 
 def outputPath(extension):
-	return os.path.join(currentPath, 'images', threeDigits(number) + '.' + extension)
+	return os.path.join(currentPath, 'images', threeDigits(number, '0') + '.' + extension)
 
 def makeNumberSVG(stringNumber):
 	modelFile = open(modelSvgPath, "r")
@@ -27,5 +27,5 @@ def makeNumberSVG(stringNumber):
 import subprocess
 
 for number in range(0,1000):	
-	makeNumberSVG(threeDigits(number))
+	makeNumberSVG(threeDigits(number,' '))
 	subprocess.call(['/usr/bin/inkscape','-z','-f',outputPath('svg'),'-w','200','-h','200','-e',outputPath('png')])
